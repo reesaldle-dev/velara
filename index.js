f112597bb3d58df125e3af16ee56f238a18eb936
reesaldle <jamesziegler@proton.me>
2024-12-02 22:53:35 -0600

    woah it works wazzup

---
+++ index.js
@@ -0,0 +1,11 @@
+const routes = [
+    { path: "/a", file: "a.html" },
+    { path: "/g", file: "art.html" },
+    { path: "/s", file: "settings.html" },
+    { path: "/p", file: "science.html" },
+    { path: "/!", file: "!.html" },
+    { path: "/", file: "index.html" },
+    { path: "/d", file: "dashboard.html" },
+    { path: "/e", file: "english.html" },
+    { path: "/-", file: "math.html" },
+];
